c1810e817d221f70cf988e2bbbce549021d273e7
AmbreThomas <embrynette@gmail.com>
2017-01-28 16:12:31 +0100

    ajout heatphase.py

--- client/Client.py
+++ client/Client.py
@@ -3,6 +3,8 @@ from Tkinter import *
 from tkMessageBox import *
 import sys
 import tkFont
+import matplotlib.pyplot as plt
+from numpy import *
 import socket
 import os
 import string
@@ -13,6 +15,45 @@ import select
 from gif import *
 
 tentacle_ip = "134.214.158.232"
+
+def heatphases(D, N, resolT, resolA):
+	ncol = 1500/resolT
+	nrow = 51/resolA
+#=================== Parsing files =====================================
+	data = []
+	print "\nouverture de %d fichiers..."%N
+	for i in xrange(1,N+1):
+		file_list = open("essai%d/results-D%d.txt"%(i, D))
+		data.append(file_list.readlines())
+		file_list.close()
+	matrix = zeros((nrow+1, ncol+1))
+	print "\nmixing the %d runs..."%N
+	for point in xrange(len(data[0])):
+		l = []
+		for essai in xrange(N):
+			l.append( data[essai][point].split(' ') )
+		matrix[ int(l[0][0])/resolA, int(l[0][1])/resolT] = sum([ int(l[k][2]) for k in xrange(N) ])/float(N)
+	print "> OK"
+	print "\ngenerating heatmap for D=%f..."%(10**(-D))
+	X = zeros((nrow+1, ncol+1))
+	Y = zeros((nrow+1, ncol+1))
+
+	for i in xrange(nrow+1):
+		for j in xrange(ncol+1):
+			X[i,j] = resolT*j+1
+			Y[i,j] = resolA*i
+	print "\n",shape(X),"\n",shape(Y),"\n",shape(matrix),"\n"
+
+
+	plt.pcolor(X,Y,matrix)
+	plt.title("Diagramme de phases des colonies satellites")
+	plt.suptitle("pour D = %f"%(10**(-D)))
+	plt.xlim(0,1500)
+	plt.ylim(0, 50)
+	plt.xlabel("Temps avant de repiquer la culture")
+	plt.ylabel("[Glucose] initiale")
+	print "saving %d.png..."%D
+	plt.savefig("%d.png"%D)
 
 def geoliste(g):
 	r = [i for i in range (0, len(g)) if not g[i].isdigit()]
